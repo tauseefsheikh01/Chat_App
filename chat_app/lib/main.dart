73cbf8b3aaab1a8d272ba5384d7235f5d2ec5cee
tauseefsheikh01 <tauseef150198@gmail.com>
2023-09-16 18:00:41 +0530

    Chat App

---
+++ chat_app/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:chat_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'package:chat_app/screens/auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 63, 17, 177)),
+      ),
+      home: const AuthScreen(),
+    );
+  }
+}
